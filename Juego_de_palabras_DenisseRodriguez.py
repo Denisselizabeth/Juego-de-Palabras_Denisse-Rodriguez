f81e7d83dcd5e5ea651de7bad428457483051287
Denisse Rodriguez <denisselizabeth@gmail.com>
2024-05-26 20:26:55 -0300

    Corrigiendo errores de validación y comparación de cada letra contra la palabra a validar

--- Juego_de_palabras_DenisseRodriguez.py
+++ Juego_de_palabras_DenisseRodriguez.py
@@ -2,43 +2,52 @@ import random
 
 # Variables: Lista de palabras y condiciones del juego
 palabras = ["innovación", "crecimiento", "liderazgo", "estrategia", "emprendimiento", "negocios", "éxito", "mercado"]
+vidas = 5
+aciertos = 0
+desaciertos = 0
 
 # Generar una palabra aleatoria
+palabra_aleatoria = list(random.choice(palabras).lower())
 #def elegir_palabra(palabras):
     #palabra_aleatoria = choice(palabras)
     #return palabra_aleatoria.upper()
-palabra_aleatoria = list(random.choice(palabras).lower())
-letras_adivinar=set(palabra_aleatoria)
+letras_adivinar=set(palabra_aleatoria)  
 
 
 # Reemplazando las letras por subguiones para mostrar al concursante
 espacios=[]
 list(filter(lambda i: espacios.append("_"),palabra_aleatoria))
 
+#validando el type de la letra
+def input_letra(letra):
+    while True:
+        if len(letra) == 1 and isinstance(letra, str):
+            #return letra
+            completar_palabra(espacios,letras_adivinar,letra)
+            break
+        else:
+            print("Ingresate un dato inválido·")
+
+#validando continuidad en el juego
+
 
 #Completando la palabra
 def completar_palabra(espacios,letras_adivinar,letra):
-    vidas=5
-    aciertos=0
-    desaciertos=0
-    while vidas>=1: #and aciertos<len(letras_adivinar):
-        print("Seguimos jugando...")
-        if letra in letras_adivinar:
-            for i in range(len(palabra_aleatoria)):
-                if palabra_aleatoria[i]==letra:
-                    espacios[i]=letra
-                    aciertos+=1
-                    print ("¡Muy bien!. Ganaste la partida. \nAhora te falta completar:{espacios}")
-                    print("Tu status en el juego es:\n Vidas: {vidas} \n Aciertos: {aciertos} \n Desaciertos: {desaciertos}")
-        else:
-            desaciertos+=1
-            vidas-=1
-            print ("¡Oh no ='(!. Perdista la partida. Aún falta por completar más letras:{espacios}")
-            print("Tu status en el juego es:\n Vidas: {vidas} \n Aciertos: {aciertos} \n Desaciertos: {desaciertos}")
+    global vidas, aciertos, desaciertos
+    if letra in letras_adivinar:
+        for i in range(len(palabra_aleatoria)):
+            if palabra_aleatoria[i]==letra:
+                espacios[i]=letra
+                aciertos+=1
+        print (f"¡Muy bien!. Ganaste la partida. \nAhora te falta completar:{espacios}")
+        print(f"Tu status en el juego es:\n Vidas: {vidas} \n Aciertos: {aciertos} \n Desaciertos: {desaciertos}")
     else:
-        print("¡Perdiste el Juego! La palabra era: {palabra_aleatoria}")
+        desaciertos+=1
+        vidas-=1
+        print (f"¡Oh no ='(!. Perdista la partida pero sigues participando. \nAún falta por completar más letras:{espacios}")
+        print(f"Tu status en el juego es:\n Vidas: {vidas} --- Aciertos: {aciertos} --- Desaciertos: {desaciertos}")
     return espacios
-
+print (espacios)
 
 #Bievenida al Juego
 print("\nEMPIEZA EL GRAN JUEGO INTERACTIVO ¡ADIVINA LA PALABRA!\n")
@@ -48,29 +57,12 @@ if respuesta.lower() == "si":
     name=input("Ingresa tu nombre: ").upper()
     print(f"Ok {name}, la palabra a adivinar tiene {len(palabra_aleatoria)} letras: {espacios}")
     print("Tienes 5 vidas. Por cada intento erróneo perderás 1. Pierdes el juego al llegar a 0 vidas.\n¡Mucha Suerte!\n")
-    letra=str(input("Ingresa una letra para completar la palabra: ").lower())
-    completar_palabra(espacios,letras_adivinar,letra)
+    while vidas>=1: #and aciertos<len(letras_adivinar):
+        print("\nSigamos jugando...")
+        letra=str(input("Ingresa una letra para completar la palabra: ").upper())
+        input_letra(letra)
+    else:
+        print(f"¡Perdiste el Juego! La palabra era: {palabra_aleatoria}")
 else:
     print("Okis ¡Hasta la próxima!")
     exit()
-
-#try / except
-#try: 
- #   print(num/num2)
-#except ZeroDivisionError:
-  #  print ("Error de división para 0")
-#except TypeError:
-   # print ("Ingresaste un número en vez de una letra")
-#except ValueError: ### SyntaxError
-    print ("Ingresaste un número en vez de una letra")
-#except Exception as e:
- #   print("Error, no se puede dividir para 0")
-  #  print (f"el error fue {e}") ### imprime el error que identifica python
-#finally:
-    #print ("Programa finalizado") 
-
-#### otra forma de validar datos
-## if radio <0
-###   raise Exception ("El radio no puede tomar valor 0")
-
-#errores a controlar: el tipo de variable a ingresar debe ser str y debe ser 1 única letra
