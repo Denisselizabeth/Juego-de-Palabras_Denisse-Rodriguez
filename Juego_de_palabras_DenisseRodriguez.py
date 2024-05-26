ae3815fecb9cbe79a3e3927bd7cbb5a0a0c7d47f
Denisse Rodriguez <denisselizabeth@gmail.com>
2024-05-26 17:33:51 -0300

    Guardo este error como referencia, después de borrar try/exit y mover variables globales y locales, el programa me queda en un loop infinito de mandarme mensajes de haber acertado y perdido

--- Juego_de_palabras_DenisseRodriguez.py
+++ Juego_de_palabras_DenisseRodriguez.py
@@ -2,9 +2,6 @@ import random
 
 # Variables: Lista de palabras y condiciones del juego
 palabras = ["innovación", "crecimiento", "liderazgo", "estrategia", "emprendimiento", "negocios", "éxito", "mercado"]
-vidas=5
-aciertos=0
-desaciertos=0
 
 # Generar una palabra aleatoria
 #def elegir_palabra(palabras):
@@ -21,23 +18,23 @@ list(filter(lambda i: espacios.append("_"),palabra_aleatoria))
 
 #Completando la palabra
 def completar_palabra(espacios,letras_adivinar,letra):
-    while vidas>=1 and aciertos<len(letras_adivinar):
+    vidas=5
+    aciertos=0
+    desaciertos=0
+    while vidas>=1: #and aciertos<len(letras_adivinar):
         print("Seguimos jugando...")
-        try:
-            if letra in letras_adivinar:
-                for i in range(len(palabra_aleatoria)):
-                    if palabra_aleatoria[i]==letra:
-                        espacios[i]=letra
-                        aciertos+=1
-                        print ("¡Muy bien!. Ganaste la partida. \nAhora te falta completar:{espacios}")
-                        print("Tu status en el juego es:\n Vidas: {vidas} \n Aciertos: {aciertos} \n Desaciertos: {desaciertos}")
-            else:
-                desaciertos+=1
-                vidas-=1
-                print ("¡Oh no ='(!. Perdista la partida. Aún falta por completar más letras:{espacios}")
-                print("Tu status en el juego es:\n Vidas: {vidas} \n Aciertos: {aciertos} \n Desaciertos: {desaciertos}")
-        except Exception as e:
-            print (f"el error fue {e}") ### imprime el error que identifica python
+        if letra in letras_adivinar:
+            for i in range(len(palabra_aleatoria)):
+                if palabra_aleatoria[i]==letra:
+                    espacios[i]=letra
+                    aciertos+=1
+                    print ("¡Muy bien!. Ganaste la partida. \nAhora te falta completar:{espacios}")
+                    print("Tu status en el juego es:\n Vidas: {vidas} \n Aciertos: {aciertos} \n Desaciertos: {desaciertos}")
+        else:
+            desaciertos+=1
+            vidas-=1
+            print ("¡Oh no ='(!. Perdista la partida. Aún falta por completar más letras:{espacios}")
+            print("Tu status en el juego es:\n Vidas: {vidas} \n Aciertos: {aciertos} \n Desaciertos: {desaciertos}")
     else:
         print("¡Perdiste el Juego! La palabra era: {palabra_aleatoria}")
     return espacios
@@ -46,19 +43,16 @@ def completar_palabra(espacios,letras_adivinar,letra):
 #Bievenida al Juego
 print("\nEMPIEZA EL GRAN JUEGO INTERACTIVO ¡ADIVINA LA PALABRA!\n")
 respuesta = input("¿Quieres jugar? \n Responde Si o No: ")
-try:
-    if respuesta.lower() == "si":
-        print("¡VAMOS! ¡Comencemos a Jugar!")
-        name=input("Ingresa tu nombre: ").upper()
-        print(f"Ok {name}, la palabra a adivinar tiene {len(palabra_aleatoria)} letras: {espacios}")
-        print("Tienes 5 vidas. Por cada intento erróneo perderás 1. Pierdes el juego al llegar a 0 vidas.\n¡Mucha Suerte!\n")
-        letra=str(input("Ingresa una letra para completar la palabra").lower())
-        completar_palabra(espacios,letras_adivinar,letra)
-    else:
-        print("¡Hasta la próxima!")
-        exit()
-except Exception as e:
-    print (f"el error fue {e}") ### imprime el error que identifica python
+if respuesta.lower() == "si":
+    print("¡VAMOS! ¡Comencemos a Jugar!")
+    name=input("Ingresa tu nombre: ").upper()
+    print(f"Ok {name}, la palabra a adivinar tiene {len(palabra_aleatoria)} letras: {espacios}")
+    print("Tienes 5 vidas. Por cada intento erróneo perderás 1. Pierdes el juego al llegar a 0 vidas.\n¡Mucha Suerte!\n")
+    letra=str(input("Ingresa una letra para completar la palabra: ").lower())
+    completar_palabra(espacios,letras_adivinar,letra)
+else:
+    print("Okis ¡Hasta la próxima!")
+    exit()
 
 #try / except
 #try: 
